9acb4770836c87b58a01dce65a890690b2d5a5d0
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2023-02-07 08:42:51 +0100

    More stats

--- querystats.php
+++ querystats.php
@@ -8,9 +8,29 @@
         str+="<line x1='10' y1='10' x2='10' y2='390' stroke='black' />";
         str+="<line x1='10' y1='390' x2='590' y2='390' stroke='black' />";
 
-        var colors=["#B52","#25B","#2B5","#BB5"];
+        var colors=["#B52","#25B","#2B5","#BB5","#2AA","#B5B","#AAA"];
+
+        // Compute sum of all cars except for xc60 and place in data as total
+        var sum=[];
+        for(var i=0;i<98;i++){
+            var csum=0;
+            for (var key in data){
+                if(key!="xc60"){
+                    var list=data[key];
+                    for(var j=0;j<list.length;j++){
+                        var item=list[j];
+                        if(item.periodi==i){
+                            csum+=item.cnt;
+                        }
+                    }
+                }
+            }
+            sum.push({cnt:csum,periodi:i});
+        }
+        data['total']=sum;
 
         var c=0;
+        var roll=8;
         for (var key in data){
             var col=colors[c];
             c++;
@@ -22,15 +42,25 @@
                 var ox=cx;
                 var oy=cy;
 
+                var val=0;
+                for(var j=0;j<roll;j++){
+                    if((i-j)>=0){
+                        val+=list[i-j].cnt;
+                    }else{
+                        val+=list[0].cnt;
+                    }
+                }
+                val=val/roll;
+
                 var cx=10+(item.periodi*6);
-                var cy=390-((item.cnt/2500)*300);
+                var cy=390-((val/2500)*300);
 
                 if(ox!=0&&oy!=0){
                     str+="<line x1='"+ox+"' y1='"+oy+"' x2='"+cx+"' y2='"+cy+"' stroke='"+col+"' stroke-width='1.5' />";
                 }
                 str+="<circle cx='"+cx+"' cy='"+cy+"' r='3' fill='"+col+"' />";
 
-            }
+            }           
         }
 
         document.getElementById("outp").innerHTML=str;
@@ -61,6 +91,8 @@ $arr['id3']=makeVehicles("%Id.3%","VW","Elbil");
 $arr['id4']=makeVehicles("%Id.4%","VW","Elbil");
 $arr['xc60']=makeVehicles("%Xc60%","Volvo","Samtlig");
 $arr['model3']=makeVehicles("%Model 3%","Tesla","Elbil");
+$arr['modelY']=makeVehicles("%Model Y%","Tesla","Elbil");
+$arr['polestar']=makeVehicles("%2%","Polestar","Elbil");
 
 echo "var data=";
 echo json_encode($arr);
